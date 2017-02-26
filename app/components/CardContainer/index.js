18bdbea0c88f9d7e1c8ec9dc61ef61d429a8112c
= <=>
2017-02-26 01:11:58 +0100

    Slight layout changes

--- app/components/CardContainer/index.js
+++ app/components/CardContainer/index.js
@@ -9,9 +9,10 @@ import styled from 'styled-components';
 
 const CardContainer = styled.section`
   border: 1px solid rgb(23.4%, 80%, 23.4%);
-  height: 300px;
-  width: 150px;
+  min-height: 150px;
+  min-width: 300px;
   margin: 10px;
   padding: 15px;
+  display: flex;
 `;
 export default CardContainer;
